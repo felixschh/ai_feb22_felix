81c2ab35835966228ee9374f7aa51fc21750cd70
Felix Schekerka <felixschekerka@gmail.com>
2022-05-30 12:53:14 +0200

    adding custom data loaders chapter

--- Chapter 03/03. MLP Regression/model.py
+++ Chapter 03/03. MLP Regression/model.py
@@ -1,9 +1,29 @@
 import torch
 import torch.nn.functional as F
-from torch import nn
+from torch import nn, softmax
 from torchsummary import summary
 
-# YOUR CODE HERE
+class Network(nn.Module):
+    def __init__(self, input):
+        super().__init__()
+        self.fc1 = nn.Linear(input, 32)
+        self.fc2 = nn.Linear(32, 16)
+        self.fc3 = nn.Linear(16, 8)
+        self.fc4 = nn.Linear(8, 1)
+
+    def forward(self, x):
+        layer1 = self.fc1(x)
+        act1 = F.relu(layer1)
+        layer2 = self.fc2(act1)
+        act2 = F.relu(layer2)
+        layer3 = self.fc3(act2)
+        act3 = F.relu(layer3)
+        layer4 = self.fc4(act3)
+        return layer4
+
+
+
+
 
 
 
--- Chapter 03/03. MLP Regression/train.py
+++ Chapter 03/03. MLP Regression/train.py
@@ -1,6 +1,4 @@
-#import the needed libraries
-
-import data_handler as dh           # yes, you can import your code. Cool!
+import data_handler as dh        
 import torch
 import torch.optim as optim
 import torch.nn as nn
@@ -8,8 +6,45 @@ import torch.nn as nn
 import matplotlib.pyplot as plt
 import numpy as np
 
-from model import #import your model here
+from model import Network
+
+net = Network(8)
+
+x_train, x_test, y_train, y_test = dh.load_data('Chapter 03/03. MLP Regression/data/turkish_stocks.csv')
+
+optimizer = optim.SGD(net.parameters(), lr=0.001)
+criterion = nn.MSELoss()
+epochs = 50
+
+train = []
+test = []
+
+for epoch in range(epochs):
+    x_train_batch, x_test_batch, y_train_batch, y_test_batch = dh.to_batches(x_train, x_test, y_train, y_test, 8)
+    running_loss = 0
+    running_loss_test = 0
+
+    for n in range(x_train_batch.shape[0]):
+        optimizer.zero_grad()
+        pred = net.forward(x_train_batch[n])
+        loss = criterion(pred, y_train_batch[n])
+
+        loss.backward()
+        optimizer.step()
+
+        with torch.no_grad():
+            test_pred = net.forward(x_test_batch)
+            test_loss = criterion(test_pred, y_test_batch)
+
+        running_loss += loss.item()
+        running_loss_test += test_loss.item()
+    train.append(running_loss/len(x_train_batch))
+    test.append(running_loss_test/len(x_test_batch))
+    print(f'{epoch + 1}/ {epochs}')
 
 
+plt.plot(train, label='train Loss')
+plt.plot(test, label='test Loss')
 
-# Remember to validate your model: model.eval .........with torch.no_grad() ......model.train
+plt.legend()
+plt.show()
